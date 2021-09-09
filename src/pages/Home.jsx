d6d627a302be3b0badfb0951d73c4c2ef59f4e2b
Natalia Fedorova <nataliafedorova1989@gmail.com>
2021-09-09 23:27:24 +0200

    add home hero and edit styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 export default function Home(){
     return(
-        <div>
-            Home
+        <div className="home-main">
+            <div className="home-text">
+            <h1>From Farm to Table</h1>
+            <p>Ecological and sustainable products only</p>
+            <p>Made with care and love for nature and planet</p>
+            </div>
         </div>
     )
 }
